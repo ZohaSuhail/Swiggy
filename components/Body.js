aac1706047c67eb25202e0c89877c8b4da9e4aef
ZohaSuhail <zohasuhail123@gmail.com>
2024-09-13 16:41:58 +0530

    main

--- components/Body.js
+++ components/Body.js
@@ -20,9 +20,7 @@ const Body = () => {
     const fetchdata = async () => {
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-            mode= 'cors',
-            headers= 
-              'Access-Control-Allow-Origin'
+     
             
         );
         const json = await data.json();
