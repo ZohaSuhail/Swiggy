040ce132d0ba4d80e8d273fde0f54b2da109d753
ZohaSuhail <157147376+ZohaSuhail@users.noreply.github.com>
2024-09-21 17:17:14 +0530

    Update Body.js

--- components/Body.js
+++ components/Body.js
@@ -18,9 +18,11 @@ const Body = () => {
 
 const url = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
     const fetchdata = async () => {
-        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
-           
-        );
+        const data = await fetch('https://swiggy-backend.vercel.app/',
+                                 {
+                                     Method:'GET'
+                                 });
+        
         const json = await data.json();
         //console.log(json);
         setlistOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
