ea99ac25cdc4b8725c3aad9fa7a575b4e7154da2
ZohaSuhail <157147376+ZohaSuhail@users.noreply.github.com>
2024-09-21 15:50:35 +0530

    Update Body.js

--- components/Body.js
+++ components/Body.js
@@ -18,11 +18,8 @@ const Body = () => {
 
 const url = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
     const fetchdata = async () => {
-        const data = await fetch(url
-            ,
-            headers:{
-                 'Access-Control-Allow-Origin':'*'
-            }
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+           
         );
         const json = await data.json();
         //console.log(json);
