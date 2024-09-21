0c24a71357ba80f4972c7208f2f8ace6efb6f9ba
ZohaSuhail <157147376+ZohaSuhail@users.noreply.github.com>
2024-09-21 15:38:23 +0530

    Update Body.js

--- components/Body.js
+++ components/Body.js
@@ -19,7 +19,7 @@ const Body = () => {
 
     const fetchdata = async () => {
         const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+            "https://cors-anywhere.herokuapp.com//www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
             headers:{
                  'Access-Control-Allow-Origin':'*'
             }
