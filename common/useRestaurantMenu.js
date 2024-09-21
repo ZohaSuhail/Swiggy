a07e123fafd85dfb86e04e249249a256e9818f80
ZohaSuhail <157147376+ZohaSuhail@users.noreply.github.com>
2024-09-21 23:37:56 +0530

    Update useRestaurantMenu.js

--- common/useRestaurantMenu.js
+++ common/useRestaurantMenu.js
@@ -10,7 +10,7 @@ const useRestaurantMenu = (resId) => {
 
 
     const fetchMenu = async () => {
-       const data=await fetch(`https://swiggy-backend-e8vx11s7g-zoha-suhails-projects.vercel.app/restaurants/${resId}`);
+       const data=await fetch(`https://swiggy-backend-6ll51eu6r-zoha-suhails-projects.vercel.app/restaurants/${resId}`);
        const json=await data.json();
        setresInfo(json.data);
     };
