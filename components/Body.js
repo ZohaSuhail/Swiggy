1af5af26ca6e3387abf5bf6fca51c6dcc6df26a6
ZohaSuhail <157147376+ZohaSuhail@users.noreply.github.com>
2024-09-21 15:49:28 +0530

    Update Body.js

--- components/Body.js
+++ components/Body.js
@@ -16,7 +16,7 @@ const Body = () => {
         fetchdata();
     }, []);
 
-const url = 'https://gobetween.oklabs.org/' + 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+const url = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
     const fetchdata = async () => {
         const data = await fetch(url
             ,
