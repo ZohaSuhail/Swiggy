814da4231c02630a6421a75fe8b69dc7ed50dada
ZohaSuhail <zohasuhail123@gmail.com>
2024-09-24 10:48:57 +0530

    updated

--- components/User.js
+++ components/User.js
@@ -5,7 +5,7 @@ const User = (props) => {
         <div className="user-card">
             <h2>Name : {name}</h2>
             <h3>Location : {location}</h3>
-            <h4>Contact : zohasuhail123@gmal.com</h4>
+            <h4>Contact : zohasuhail123@gmail.com</h4>
         </div>
     )
 }
