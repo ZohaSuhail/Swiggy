e5a4be328e5def39c198d633b977a987be54dce8
ZohaSuhail <zohasuhail123@gmail.com>
2024-09-24 10:47:21 +0530

    updated

--- components/About.js
+++ components/About.js
@@ -1,12 +1,12 @@
 import User from "./User";
-import UserClass from "./UserClass";
+
 const About = () => {
     return (
     <div>
         
-        <User name={"Zoha function"} location={"Varanasi"} />
+        <User name={"Zoha Suhail"} location={"Varanasi"} />
         
-        <UserClass name={"Zoha class"} location={"Varanasi"}  />
+        
     </div>
     ); 
 
